6c7f51de9e3a9432ca98abf8dfde6313618c520d
Mr Bhardwaj <mrbhardwaj@Mrs-MacBook-Pro.local>
2026-06-23 18:33:29 +0530

    Fun Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { lazy, Suspense } from "react";
+import { lazy } from "react";
 import "./App.css";
 
-const CharacterModel = lazy(() => import("./components/Character"));
 const DateComponent = lazy(() => import("./components/Date"));
-const MainContainer = lazy(() => import("./components/MainContainer"));
-import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
   return (
