5bf36ce9ddd897f3181298be6e7c62e05af120f4
Mr Bhardwaj <mrbhardwaj@Mrs-MacBook-Pro.local>
2026-06-23 17:54:13 +0530

    Bug resolved

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -14,13 +14,13 @@ import {
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
   "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
+  "/images/firebase.png",
+  "/images/crossPlatform.png",
+  "/images/graphQL.png",
+  "/images/nativeReact.png", 
   "/images/typescript.webp",
   "/images/javascript.webp",
+   "/images/npm.png",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
